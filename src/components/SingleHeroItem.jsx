422d29f47c565cc827919fc85d10d96134ede442
wurieBah27 <wuriebah27@gmail.com>
2025-05-10 13:10:59 +0400

    fixing the category section on the home page

--- src/components/SingleHeroItem.jsx
+++ src/components/SingleHeroItem.jsx
@@ -10,23 +10,23 @@ const SingleHeroItem = ({ data = {} }) => {
       <Link to={`/product/${id}`}>
         {/* <div class="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ..."></div> */}
 
-        <div className="flex h-44 gap-3 bg-gradient-to-r from-gray-700 from-10% via-gray-500 via-30% to-gray-100 to-90% px-2 py-4 sm:h-56 sm:px-6">
+        <div className="text-content flex h-44 gap-3 from-gray-700 from-10% via-gray-600 via-30% to-gray-300 to-90% px-2 py-4 dark:bg-gradient-to-r sm:h-60 sm:max-h-60 sm:px-6">
           <div className="flex-[2]">
             <div className="max-h-40 sm:max-h-56 sm:w-[65%]">
-              <h4 className="py-1 text-xl text-gray-100 dark:text-teal-500 sm:text-2xl">
+              <h4 className="py-1 text-xl text-gray-900 dark:text-gray-100 max-[340px]:text-base sm:text-2xl">
                 {name}
               </h4>
-              <h2 className="line-clamp-2 pb-3 text-xs font-medium text-gray-200 dark:text-gray-500 sm:line-clamp-3">
+              <h2 className="line-clamp-2 text-xs font-medium text-gray-600 dark:text-gray-300 sm:line-clamp-3">
                 {descriptions}
                 <br />
               </h2>
-              <span className="mt-2 flex flex-wrap items-baseline font-bold text-slate-100 dark:text-white">
+              <span className="mt-2 flex flex-wrap items-baseline font-bold text-gray-700 dark:text-white">
                 {" "}
                 <span>AED {(+price - itemPrice).toFixed(2)} </span>
                 <span className="text-sm font-semibold text-[#82AE04] sm:text-xl">
                   {discount > 0 && (
                     <span className="inline-block w-full p-1 text-xs sm:text-sm">
-                      <span className="mr-1 text-gray-100 line-through dark:text-gray-100">
+                      <span className="mr-1 text-gray-700 line-through dark:text-gray-100">
                         AED {price}
                       </span>{" "}
                       <span className="mr-1 text-red-600 dark:text-red-400">
@@ -37,19 +37,16 @@ const SingleHeroItem = ({ data = {} }) => {
                   )}
                 </span>
               </span>
-              <Link
-                to={"/cart"}
-                className="btn-primary text mt-1 flex max-w-max items-center justify-center rounded-lg bg-teal-700 px-6 py-2 text-center text-sm font-bold text-gray-100 shadow-2xl transition-all hover:translate-y-1 hover:bg-teal-600 focus:ring-4"
-              >
+              <span className="btn-primary mt-2 flex max-w-max items-center justify-center rounded-lg bg-teal-700 px-6 py-2 text-center text-sm font-bold text-gray-100 shadow-2xl transition-all hover:translate-y-1 hover:bg-teal-600 focus:ring-4 max-[350px]:px-4 max-[350px]:py-1">
                 Order Now
-              </Link>
+              </span>
             </div>
           </div>
-          <div className="flex-1 overflow-hidden rounded-full sm:max-h-56">
+          <div className="flex flex-1 items-center justify-center overflow-hidden rounded-full sm:max-h-56">
             <img
               src={imgUrls?.at(0) || "/take-out-cup2.jpg"}
               alt="Hero image"
-              className="h-full w-full object-cover"
+              className="w-full rounded-full"
             />
           </div>
         </div>
