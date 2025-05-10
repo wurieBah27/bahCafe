4fc7a5992e92a2798b84895d6e6a9956f8643fbb
wurieBah27 <wuriebah27@gmail.com>
2025-05-10 13:18:47 +0400

    fixing the category section on the home page

--- src/sections/CategoriesHeader.jsx
+++ src/sections/CategoriesHeader.jsx
@@ -2,7 +2,7 @@ const CategoriesHeader = ({ category }) => {
   return (
     <div className="mb-3 mt-6 px-4">
       <div>
-        <div className="flex items-center justify-between rounded-md border-b-2 border-gray-900 p-3 dark:border-gray-700 dark:bg-gray-700">
+        <div className="flex items-center justify-between border-b-2 border-b-gray-400 p-3 dark:rounded-md dark:border-b-gray-700 dark:bg-gray-700">
           <div className="">
             <h4 className="text-center text-2xl font-bold uppercase text-teal-500 dark:text-teal-300 md:text-3xl">
               {category}
