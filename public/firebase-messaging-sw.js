162e2dd0644d996548c00929d11c416db5d465a8
wurieBah27 <wuriebah27@gmail.com>
2025-05-12 05:31:42 +0400

    Adding push notifications to the website

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,17 +5,23 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js",
 );
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBI6CBG1jHAcxwEeTzRpun6WWFwg5wQ6dg",
-  authDomain: "cafe-management-website.firebaseapp.com",
-  projectId: "cafe-management-website",
-  storageBucket: "cafe-management-website.firebasestorage.app",
-  messagingSenderId: "60197197886",
-  appId: "1:60197197886:web:b43a33e560a1c1265d196f",
-  measurementId: "G-65QBFTCVGJ",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBI6CBG1jHAcxwEeTzRpun6WWFwg5wQ6dg",
+//   authDomain: "cafe-management-website.firebaseapp.com",
+//   projectId: "cafe-management-website",
+//   storageBucket: "cafe-management-website.firebasestorage.app",
+//   messagingSenderId: "60197197886",
+//   appId: "1:60197197886:web:b43a33e560a1c1265d196f",
+//   measurementId: "G-65QBFTCVGJ",
+// };
 
-firebase.initializeApp(firebaseConfig);
+const messagingSenderId = "60197197886";
+
+firebase.initializeApp({
+  messagingSenderId: messagingSenderId,
+});
+
+// firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
