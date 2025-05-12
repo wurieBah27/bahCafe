ba3b5cc3906baca0bc47f5d898d814e11e2b25f9
wurieBah27 <wuriebah27@gmail.com>
2025-05-12 05:36:14 +0400

    Adding push notifications to the website

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,16 +5,6 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js",
 );
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBI6CBG1jHAcxwEeTzRpun6WWFwg5wQ6dg",
-//   authDomain: "cafe-management-website.firebaseapp.com",
-//   projectId: "cafe-management-website",
-//   storageBucket: "cafe-management-website.firebasestorage.app",
-//   messagingSenderId: "60197197886",
-//   appId: "1:60197197886:web:b43a33e560a1c1265d196f",
-//   measurementId: "G-65QBFTCVGJ",
-// };
-
 const messagingSenderId = "60197197886";
 
 firebase.initializeApp({
